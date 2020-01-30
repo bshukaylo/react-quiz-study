8039a78ac7add470ef4fe0365f8e0b66a999bac9
Boris Shukaylo <bshukaylo@gmail.com>
2020-01-30 22:26:21 +0600

    added question

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classes from './ActiveQuiz.module.css';
+
+const ActiveQuiz = props => (
+    <div className={classes.ActiveQuiz}>
+        <p className={classes.Question}>
+            <span>
+                <strong>
+                    2.
+                </strong>&nbsp;
+                Kak dela?
+            </span>
+            <small>4 from 12</small>
+        </p>
+
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    </div>
+);
+
+export default ActiveQuiz;
